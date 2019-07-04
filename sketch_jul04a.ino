f79b1af160a588c22439d65185a51527f410ab84
STROMWX <52492205+STROMWX@users.noreply.github.com>
2019-07-04 19:49:22 +0800

    1-9变换

--- sketch_jul04a.ino
+++ sketch_jul04a.ino
@@ -1,51 +1,45 @@
-#include <Morse.h>
-Morse morse(13);
-
-void setup() {
-  Serial.begin (9600);    //设置通信波特率    
-}
-
-void loop() {
-char str[1000];               //定义数组，数组容量1000
-
- int temp=0;                 //表示接收状态
- int n=0;
-if (Serial.available()>0)
+void setup()
 {
-  temp=1;
- Serial.readBytes(str,1000);  //读取字符串
- delay(2);  //延迟
- 
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+  
+   digitalWrite(6,LOW);
+  delay(100);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  delay(100);
+  digitalWrite(6,HIGH);
+  delay(100);//预设初始显示为0
+  
+
 }
-                          
-while (n<1000)                  //对字符串中的每一个字符进行选择
+int income=0;
+void loop()
 {
-  switch(str[n])
-  {
+  if(Serial.available()>0)
+    {
+    income=Serial.read();
     
-    case' ':morse.dash();morse.dash();morse.dash();morse.dash();break;    
-    case'a':morse.dot();morse.dash();break;
-    case'b':morse.dash();morse.dot();morse.dot();morse.dot();break;
-    case'c':morse.dash();morse.dot();morse.dash();morse.dot();break;
-    case'd':morse.dash();morse.dot();morse.dot();break;
-    case'e':morse.dot();break;
-    case'f':morse.dot();morse.dot();morse.dash();morse.dot();break;
-    case'g':morse.dash();morse.dash();morse.dot();break;
-    case'h':morse.dot();morse.dot();morse.dot();morse.dot();break;
-    case'i':morse.dot();morse.dot();break;
-    case'j':morse.dot();morse.dash();morse.dash();morse.dash();break;
-    case'k':morse.dash();morse.dot();morse.dash();break;
-    case'l':morse.dot();morse.dash();morse.dot();morse.dot();break;            
-    case'm':morse.dash();morse.dash();break;
-    case'n':morse.dash();morse.dot();break;
-    case'o':morse.dash();morse.dash();morse.dash();break;
-    case'p':morse.dot();morse.dash();morse.dash();morse.dot();break;          
-    case'q':morse.dash();morse.dash();morse.dot();morse.dash();break;
-    case'r':morse.dot();morse.dash();morse.dot();break;
-    case's':morse.dot();morse.dot();morse.dot();break;
-    case't':morse.dash();break;
-    case'u':morse.dot();morse.dot();morse.dash();break;
-    case'v':morse.dot();morse.dot();morse.dot();morse.dash();break;
-    case'w':morse.dot();morse.dash();morse.dash();break;
-    case'x':morse.dash();morse.dot();morse.dot();morse.dash();break;
-    case'y':morse.dash();morse.dot();morse.dash();morse.dash();break;
+    income=income-'0';
+    Serial.println(income,DEC);
+    digitalWrite(6,HIGH);
+    delay(100);
+    
+    digitalWrite(2,income&0x1?HIGH:LOW);
+    digitalWrite(3,(income>>1)&0x1?HIGH:LOW);
+    digitalWrite(4,(income>>2)&0x1?HIGH:LOW);
+    digitalWrite(5,(income>>3)&0x1?HIGH:LOW);
+    Serial.println(income&0x1);
+    /*digitalWrite(2,HIGH);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);*/
+    delay(100);
+  }
+}
